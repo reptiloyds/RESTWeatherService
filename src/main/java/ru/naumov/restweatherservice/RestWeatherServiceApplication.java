8cc664fad79f9b8c687fe9a5b7af35566bc22b64
Ilya Naumov <hairon20152003@mail.ru>
2025-09-05 15:01:50 +0300

    Init

---
+++ src/main/java/ru/naumov/restweatherservice/RestWeatherServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.naumov.restweatherservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWeatherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWeatherServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/naumov/restweatherservice/RestWeatherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.naumov.restweatherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWeatherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
